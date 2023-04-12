46b5e0e7d9e358afed60c3361aeb3f5267f5f3c4
Mainak <developer.mainakbanerjee@gmail.com>
2023-04-12 12:03:42 +0530

    used swiggys database

--- src/components/restaurantList/RestaurantList.jsx
+++ src/components/restaurantList/RestaurantList.jsx
@@ -1,19 +1,17 @@
 import RestaurantCard from "./RestaurantCard";
+import { RestaurantData } from "../../config";
 
 const RestaurantList = () => {
+  console.log(RestaurantData[0]);
   return (
     <div>
       <h1>Restaurant List</h1>
       <div className="restaurant-card-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        <RestaurantCard restaurantOptions={RestaurantData[0]?.data} />
+        <RestaurantCard restaurantOptions={RestaurantData[1]?.data} />
+        <RestaurantCard restaurantOptions={RestaurantData[2]?.data} />
+        <RestaurantCard restaurantOptions={RestaurantData[3]?.data} />
+        <RestaurantCard restaurantOptions={RestaurantData[4]?.data} />
       </div>
     </div>
   );
