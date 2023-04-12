8e8a52202a29be52108612c97b77c4c499e9346c
Mainak <developer.mainakbanerjee@gmail.com>
2023-04-12 10:55:50 +0530

    created app layout comments

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
+/**App Layout
+ * Header
+ *  Logo
+ *  Nav Bar
+ *  Cart
+ * Body
+ *  ResturantLists
+ *    ResturantCard
+ *      Images
+ *      Name
+ *      Ratings
+ *      Cuisins
+ *      Distance/Address
+ * Footer
+ *  Copyright (c)
+ */
+
 const App = () => {
-  return <h2>Zwigato Food</h2>;
+  return (
+    <>
+      <h2>Zwigato Food</h2>;{/* {This is a comment} */}
+    </>
+  );
 };
 
 export default App;
