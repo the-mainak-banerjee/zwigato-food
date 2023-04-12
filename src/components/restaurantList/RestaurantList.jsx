14ba8737233f3f309593f50e25bded0d19028f6c
Mainak <developer.mainakbanerjee@gmail.com>
2023-04-12 12:17:09 +0530

    feat: implemented basic search functionality

--- src/components/restaurantList/RestaurantList.jsx
+++ src/components/restaurantList/RestaurantList.jsx
@@ -1,17 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
 import { RestaurantData } from "../../config";
+import { useState } from "react";
+
+const filerdData = (restaurants, searchTerm) => {
+  return restaurants.filter((restaurant) => {
+    return restaurant.data.name
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+  });
+};
 
 const RestaurantList = () => {
-  console.log(RestaurantData[0]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [restaurants, setRestaurants] = useState(RestaurantData);
   return (
     <div>
-      <h1>Restaurant List</h1>
+      <div className="restaurant-search-container">
+        <input
+          className="input"
+          type="text"
+          placeholder="search for a restaurant"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button
+          className="button"
+          onClick={() => setRestaurants(filerdData(restaurants, searchTerm))}
+        >
+          search
+        </button>
+      </div>
       <div className="restaurant-card-container">
-        <RestaurantCard restaurantOptions={RestaurantData[0]?.data} />
-        <RestaurantCard restaurantOptions={RestaurantData[1]?.data} />
-        <RestaurantCard restaurantOptions={RestaurantData[2]?.data} />
-        <RestaurantCard restaurantOptions={RestaurantData[3]?.data} />
-        <RestaurantCard restaurantOptions={RestaurantData[4]?.data} />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurantOptions={restaurant?.data} />
+        ))}
       </div>
     </div>
   );
