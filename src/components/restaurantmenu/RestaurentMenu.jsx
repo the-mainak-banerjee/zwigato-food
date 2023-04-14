53606da733479294c877ade4a2f4dca94a7b3d51
Mainak <developer.mainakbanerjee@gmail.com>
2023-04-14 13:57:20 +0530

    feat: added custom hook in restaurent menu

--- src/components/restaurantmenu/RestaurentMenu.jsx
+++ src/components/restaurantmenu/RestaurentMenu.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import useRestaurent from "../../utils/useRestaurent";
 
 const RestaurentMenu = () => {
   const { id } = useParams();
+  const restaurant = useRestaurent(id);
+
+  if (!restaurant) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-      <div>Restaurent Menu of id {id}</div>
+      <h2>{restaurant?.name}</h2>
+      <img
+        src={`${process.env.REACT_APP_IMAGE_URL}${restaurant.cloudinaryImageId}`}
+      />
+      <p>
+        {restaurant?.locality}, {restaurant?.city}
+      </p>
     </>
   );
 };
